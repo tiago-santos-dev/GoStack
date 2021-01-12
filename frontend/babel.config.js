9b234d984c4a5e5b49479dc57f55ca16ae709daa
Tiago Rodrigues <trodrigues322@gmail.com>
2021-01-12 20:21:34 -0300

    Initial strucuture, Add babel(presets) e webpack

---
+++ frontend/babel.config.js
@@ -0,0 +1,9 @@
+module.exports =  {
+    presets: [
+        '@babel/preset-env',//Converte funcionalidades do JS de acordo 
+                            //com o ambiente que a aplicação está rodando.
+
+        '@babel/preset-react' // Entende o html dentro do JS e converter de
+                            //maneira que o browser entenda. 
+    ]
+}
