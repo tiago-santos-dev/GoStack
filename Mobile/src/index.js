d5328512e110487193ac40473c359086111a6934
TiagoRodrigues456 <trodrigues322@gmail.com>
2021-02-09 07:43:08 -0300

    Listando projetos da API - (Add Axios)

---
+++ Mobile/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Text, StyleSheet, StatusBar, SectionList } from 'react-native';
+
+// Não Possui valor semântico (significado)
+// Não Possui estilização própria
+// Todos os componentes possuem por padrão "display : flex"
+
+// View: div, footer, header, mais, aside, Section
+// Text: p, span, strong, h1, h2, h3
+
+export default function App(){
+    return(
+        <>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+        <View style={styles.container}>
+            <Text style={styles.title}>Hello GoStack</Text>
+        </View>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        backgroundColor: '#7159c1',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title : {
+        color: '#FFF',
+        fontSize: 32,
+        fontWeight : 'bold',
+    }
+});
---
+++ Mobile/src/services/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL=''
+});
+
+export default api;
+
+/**
+ * BaseURL
+ * IOS / Emulador : localhost
+ * IOS / Físico : IP da Máquina
+ * Android / Emulador : localhost (adb reverse)
+ * Android / Emulador : 10.0.2.2 (Android Studio)
+ * Android / Emulador : 10.0.3.2 (Genymotion)
+ * Android / Físico : Ip da Máquina
+ */
