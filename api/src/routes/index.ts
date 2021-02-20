f8e90e51cc04a2f12d2310a5b6edfb1d1d53f5d0
TiagoRodrigues456 <trodrigues322@gmail.com>
2021-02-20 18:25:29 -0300

    Configurando Debug - vscode

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,3 +1,19 @@
 import { Router } from 'express';
+
 const routes = Router();
+
+routes.get('/', (request, response) => {
+    return response.json({ message: 'Hello GoStack' });
+});
+
+routes.post('/users', (request, response) => {
+    const { name, email } = request.body;
+
+    const user = {
+        name,
+        email,
+    };
+    return response.json(user);
+});
+
 export default routes;
