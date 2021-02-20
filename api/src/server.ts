38e306c4dc31bfd02b9158440e634b287d0ea66c
TiagoRodrigues456 <trodrigues322@gmail.com>
2021-02-20 17:54:45 -0300

    Configurando padrões de projeto com ESLint, Prettier e EditorConfig

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request , response) => {
-    return response.json({message : 'Hello GoStack'});
+     return response.json({message : 'Hello GoStack'});
 });
 
 app.listen(3333, () => {
